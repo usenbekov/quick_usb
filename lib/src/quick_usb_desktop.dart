5c75ecc226c9b531e47bf73cf1f89e437deecafc
usenbekov <altosh.mail@gmail.com>
2025-01-28 17:04:24 +0600

    Fix compatibility issue on macOS arm64

--- lib/src/quick_usb_desktop.dart
+++ lib/src/quick_usb_desktop.dart
@@ -19,7 +19,11 @@ class QuickUsbWindows extends _QuickUsbDesktop {
 
 class QuickUsbMacos extends _QuickUsbDesktop {
   QuickUsbMacos() {
-    _libusb = Libusb(DynamicLibrary.open('libusb-1.0.23.dylib'));
+    if (Platform.version.contains('arm64')) {
+      _libusb = Libusb(DynamicLibrary.open('libusb-1.0.23_arm64.dylib'));
+    } else {
+      _libusb = Libusb(DynamicLibrary.open('libusb-1.0.23.dylib'));
+    }
   }
 }
 
